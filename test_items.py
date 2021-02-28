270bb85e2caf54ceef3ecd6241dc349e5e640d14
martantong <martantong@gmail.com>
2021-02-28 19:54:57 +0300

    last_finish

--- test_items.py
+++ test_items.py
@@ -1,5 +1,4 @@
-
-
 def test_is_add_button_presents(browser):
     browser.get(" http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    browser.find_element_by_class_name('btn-add-to-basket')
+    btn = browser.find_elements_by_class_name('btn-add-to-basket')
+    assert btn, "Кнопка не найдена"
